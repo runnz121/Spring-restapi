3eccce551aaaf81f0c39de2549935b76d2e61dd6
runnz <runnz121@gmail.com>
2020-12-27 17:02:58 +0900

    add comment

--- src/main/java/restfulwebservice/applicationname/helloworld/HelloWorldBean.java
+++ src/main/java/restfulwebservice/applicationname/helloworld/HelloWorldBean.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 @Data//lombok 사용위함(setter, getter)
 @AllArgsConstructor//private String message를 사용하기 위한 생성자를 대신해줌
+                    //public HelloWorldBean(String message) {
+                    //      this.message = message; }
 @NoArgsConstructor//디폴트 생성자 만들기
 public class HelloWorldBean {
 
--- src/main/java/restfulwebservice/applicationname/helloworld/HelloWorldController.java
+++ src/main/java/restfulwebservice/applicationname/helloworld/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
     //GET method
-    //hello-world ->Endpoint
+    //hello-world ->Endpoint : 호출 되는 페이지
     //@RequestMapping ->restAPI 쓰기전에 사용 = (method=RequestMethod.get, path="/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -17,7 +17,7 @@ public class HelloWorldController {
 
     }
 
-    //클래스에 값을 받아 반환
+    //클래스에 값을 받아 반환, 자바 bena형태 https://elfinlas.github.io/2017/10/31/javabean/
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() { //HelloWorldBean으로 값을 반환
         return new HelloWorldBean("Hello world"); //HelloWorldBean 클래스를 통해서 반환(자바 bean 형태)-> json 형태로 반환 받음
@@ -25,9 +25,9 @@ public class HelloWorldController {
     }
 
     //pathvariable 사용
-    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")    //@Pathvaraible이라고 지정한것과 같은 이름 써줌
     public HelloWorldBean helloWorldBean(@PathVariable String name) { //name을 위의 가변 데이터 값으로 넣어줌
-        return new HelloWorldBean(String.format("Hello World, %s", name)); //url 뒤에 해당 name을 입력하면 반환시 그 name이 출력됨
+        return new HelloWorldBean(String.format("Hello World, %s", name)); //url 뒤에 해당 name을 입력하면 반환시 그 name이 출력됨 , string format은 단지 형식 문자열 출력을 위함
     }
 
 
